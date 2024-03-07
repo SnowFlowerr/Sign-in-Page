19d720772e5ab293170d2890b6df90a4ec5b59e4
Bhudeo Krit <Bhudeokrit@gmail.com>
2024-03-08 02:02:38 +0530

    responsiveui

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <div className={styles.navbar}>
-            <ul className={styles.menu}>
-                <li className={styles.item}><Link to='/'>Home</Link></li>
-                <li className={styles.item}><Link to='/about'>About</Link></li>
-                <li className={styles.item}><Link to='/contact'>Contact</Link></li>
-                <li className={styles.item}><Link to='/signin'>Signin</Link></li>
-            </ul>
+            <div className={styles.menu}>
+                <span className={styles.item}><Link to='/'>Home</Link></span>
+                <span className={styles.item}><Link to='/about'>About</Link></span>
+                <span className={styles.item}><Link to='/contact'>Contact</Link></span>
+                <span className={styles.item}><Link to='/signin'>Signin</Link></span>
+            </div>
         </div>
     )
 }
